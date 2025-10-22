4b381c548f4705ca43efbd10c4862b08bb947026
shogo <0814.sho.mura@gmail.com>
2025-10-22 21:59:11 +0900

    complete this project. waving not parallel

--- projects/dot.js
+++ projects/dot.js
@@ -1,8 +1,62 @@
+let block = [];
+let width=400,height=400;
+let block_size = 20;
+let num_block = width/block_size;
+let time;
+
+let tile = []
+
+ 
+class Tile{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.monoc = random(50,200);//mono color
+        this.color = this.monoc;
+        this.size = block_size;
+        this.colorR = random(150,255);
+        this.colorG = random(150,255);
+        this.colorB = random(150,255);
+    }
+
+    display(){
+        noStroke();
+        fill(this.colorR,this.colorG,this.colorB);
+        square(this.x,this.y,this.size);
+    }
+
+    r_update(t){
+        let phasex = this.x*0.03;
+        let phasey = this.y*0.005;
+        let multi = phasex*phasey*0.1;
+        this.size = block_size*sin((TWO_PI*t-multi-phasey));
+    }
+}
+
+
+
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width,height);
+    for(let i=0;i<width;i+=block_size){
+        for(let j=0;j<height;j+=block_size){
+            block.push([i,j]);
+            let t = new Tile(i,j);
+            tile.push(t);
+        }
+    }
+    //console.log(tile);
 }
 
 function draw() {
-    background(220);
+    background(250);
     // Your drawing code here
+    time = millis()/1000;
+
+    tile.forEach(function(t){
+        //console.log(t);
+        t.r_update(time);
+        t.display(t);
+    })
+
 }
